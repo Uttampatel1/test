e7b171fb975050691202caf6ffad8414569d4302
Uttam Patel <86312108+Uttampatel1@users.noreply.github.com>
2024-01-30 18:27:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ def setup_api_handler(uri, handler):
     
 # === Usage ======
 class HelloHandler(RequestHandler):
-    def set_default_headers(self):
-        self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-        self.set_header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    # def set_default_headers(self):
+    #     self.set_header("Access-Control-Allow-Origin", "*")
+    #     self.set_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    #     self.set_header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
     def options(self):
         # Handle pre-flight OPTIONS request
